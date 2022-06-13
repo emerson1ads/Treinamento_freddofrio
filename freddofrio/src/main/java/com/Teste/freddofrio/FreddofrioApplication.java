88505d978123927162b77f6faf6753cbfd4a51cd
Emerson <emerson.ads1010@gmail.com>
2022-06-13 12:02:19 -0300

    ola

---
+++ freddofrio/src/main/java/com/Teste/freddofrio/FreddofrioApplication.java
@@ -0,0 +1,13 @@
+package com.Teste.freddofrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreddofrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreddofrioApplication.class, args);
+	}
+
+}
---
+++ freddofrio/src/test/java/com/Teste/freddofrio/FreddofrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teste.freddofrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreddofrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
